f6990affa68e4927be97b48381a6de0736099715
Przemysław Szyszka <przemeklo@o2.pl>
2015-02-10 11:22:13 +0100

    #1 - Remove modules

--- lib/rate-limiter.rb
+++ lib/rate-limiter.rb
@@ -1,13 +1,9 @@
-require "rate/limiter/version"
-
-module Rate
-  module Limiter
-    def call(env)
-      [
-        "200",
-        {"Content-Type" => "text-html"},
-        ["Test"]
-      ]
-    end
+class RateLimiter
+  def call(env)
+    [
+      "200",
+      {"Content-Type" => "text-html"},
+      ["Test"]
+    ]
   end
 end
